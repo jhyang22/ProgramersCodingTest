bee56b84e97c3e6c9363d0ceeb8c05135ba2a2c1
jhyang22 <jaeho9229@gmail.com>
2025-07-19 06:57:53 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.07 ms, Memory: 88.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String my_string) {
+
+        StringBuffer strBuf = new StringBuffer(my_string);
+
+        return strBuf.reverse().toString();
+    }
+}
