e013f86236230bf1e5d435a08ca556332a239285
jhyang22 <jaeho9229@gmail.com>
2025-08-29 22:27:16 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 85.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(int[] array) {
+        int[] answer = new int[2];
+        int maxNum = Integer.MIN_VALUE;
+        int index = 0;
+        
+        for(int i = 0; i < array.length; i++) {
+            if(array[i] > maxNum) {
+                maxNum = array[i];
+                index = i;
+            }
+        }
+        
+        answer[0] = maxNum;
+        answer[1] = index;
+        
+        return answer;
+    }
+}
